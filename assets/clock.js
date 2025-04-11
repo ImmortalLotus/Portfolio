48377a949bea7fb2619f6705f55694216c463575
J. Thomas <jttbsb@gmail.com>
2025-04-10 21:24:49 -0300

    update front

---
+++ assets/clock.js
@@ -0,0 +1,25 @@
+import { animate,utils,createTimer } from './anime.esm.js';
+
+const [ $time, $count ] = utils.$('.value');
+
+function formatTimeFromMilliseconds(ms) {
+  const totalSeconds = Math.floor(ms / 1000);
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = totalSeconds % 60;
+
+  const paddedHours = String(hours).padStart(2, '0');
+  const paddedMinutes = String(minutes).padStart(2, '0');
+  const paddedSeconds = String(seconds).padStart(2, '0');
+
+  return `${paddedHours}:${paddedMinutes}:${paddedSeconds}`;
+}
+
+
+createTimer({
+  duration: 1000,
+  loop: true,
+  frameRate: 30,
+  onUpdate: self => $time.innerHTML = formatTimeFromMilliseconds(self.currentTime),
+  onLoop: self => $count.innerHTML = self._currentIteration
+});
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+module.exports = {
+  content: ["./**/*.html"],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
